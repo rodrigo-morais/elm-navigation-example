f73568ec581d64be26d6f6e6195d8432ac1604e2
Rodrigo Morais <morais.rm@gmail.com>
2016-08-04 11:51:30 -0300

    Removing route's logic from Main file to a specific file called Routes.

--- Main.elm
+++ Main.elm
@@ -31,67 +31,11 @@ import Html.Attributes exposing (style, href, id)
 import Html.App
 
 
-import String
 import Navigation
-import UrlParser exposing (..)
+import String
 
 
-{--
-  - Creating the routes which the app has to respond
---}
-type Route
-  = Route1
-  | Route2
-  | NotFound
-
-
-{--
-  - Looking for the route and if it exists will be returned
---}
-matchers : Parser (Route -> a) a
-matchers =
-  oneOf
-    [ format Route1 (s "")
-    , format Route2 (s "route2")
-    , format Route1 (s "route1")
-    ]
-
-
-{--
-  - Get the location from Navigation when it changes, and after parse it
-  - If the location exist in matchers the route will be returned otherwise an error will be
-  - How Result works:
-    - If we have a success then it return the right side which is our Route
-    - If we have a fail it returns the left side which is our String. In this case the String is an error
---}
-hashParser : Navigation.Location -> Result String Route
-hashParser location =
-  location.hash
-  |> String.dropLeft 2
-  |> parse identity matchers
-
-
-{--
-  - The Navigation package waits for a parser for the current location
---}
-parser: Navigation.Parser (Result String Route)
-parser =
-  Navigation.makeParser hashParser
-
-
-{--
-  - In the end is necessarie to handle with the return of parse
-  - It will return a Result with our route or an error
-  - In this case we return a route if we can match it or a NotFound route if not
---}
-routeFromResult : Result String Route -> Route
-routeFromResult result =
-  case result of
-    Ok route ->
-      route
-
-    Err _ ->
-      NotFound
+import Routes exposing (..)
 
 
 {--
